139b72cc668b225c148e09fb0a9dc9ef74d6965e
David Hutchison <dhutchison86+github@googlemail.com>
2024-03-07 00:09:27 +0000

    feat(tests): coverage from testcontainer run

--- Dockerfile
+++ Dockerfile
@@ -16,13 +16,18 @@ RUN addgroup payara && \
 USER payara
 WORKDIR ${PAYARA_HOME}
 
-# Default command to run
-ENTRYPOINT ["java", "-XX:+UseContainerSupport", "-XX:MaxRAMPercentage=90.0", "-jar", "payara-micro.jar"]
-CMD ["--deploymentDir", "/opt/payara/deployments"]
-
 # Download specific
 ARG PAYARA_VERSION="6.2024.2"
 ENV PAYARA_VERSION="$PAYARA_VERSION"
 RUN wget --no-verbose -O ${PAYARA_HOME}/payara-micro.jar https://repo1.maven.org/maven2/fish/payara/extras/payara-micro/${PAYARA_VERSION}/payara-micro-${PAYARA_VERSION}.jar
 
+# Copy in the war
 COPY target/experiments.war ${DEPLOY_DIR}/
+
+# Default command to run
+ENV JAVA_OPTS=""
+ENV PAYARA_OPTS=""
+
+# Default command to run
+# ENTRYPOINT ["java", "-XX:+UseContainerSupport", "-XX:MaxRAMPercentage=90.0", "-jar", "payara-micro.jar", "--deploymentDir", "/opt/payara/deployments"]
+ENTRYPOINT java -XX:+UseContainerSupport -XX:MaxRAMPercentage=90.0 ${JAVA_OPTS} -jar payara-micro.jar --deploymentDir "/opt/payara/deployments" ${PAYARA_OPTS}
--- src/test/java/com/devwithimagination/microprofile/experiments/it/AbstractBaseIT.java
+++ src/test/java/com/devwithimagination/microprofile/experiments/it/AbstractBaseIT.java
@@ -1,5 +1,12 @@
 package com.devwithimagination.microprofile.experiments.it;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jacoco.core.tools.ExecDumpClient;
+import org.jacoco.core.tools.ExecFileLoader;
+import org.junit.jupiter.api.AfterAll;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
@@ -17,9 +24,15 @@ public abstract class AbstractBaseIT {
 
     static {
         API_CONTAINER = new GenericContainer<>(DockerImageName.parse("devwithimagination/microprofile-experiments:latest"))
-                .withExposedPorts(8080)
-                .waitingFor(Wait.forHttp("/health").forStatusCode(200));
-        // I've not implemented a healthcheck, but there is a strategy for waiting for the container to be healthy
+                .withExposedPorts(8080, 6300)
+                .waitingFor(Wait.forHttp("/health").forStatusCode(200))
+                /*
+                 * I've not implemented a healthcheck, but there is a strategy for waiting for the container to be healthy,
+                 * but in this case they are the same end goal
+                 */
+                .withFileSystemBind("./target/jacoco-agent", "/opt/jacoco/agent", BindMode.READ_ONLY)
+                .withEnv("JAVA_OPTS", "-javaagent:/opt/jacoco/agent/org.jacoco.agent-runtime.jar=output=tcpserver,address=*,port=6300")
+                .withEnv("PAYARA_OPTS", "--noHazelcast");
 
         API_CONTAINER.start();
 
@@ -30,5 +43,21 @@ public abstract class AbstractBaseIT {
         System.out.println(baseUrl);
 
         RestAssured.baseURI = baseUrl;
+    }
+
+    @AfterAll
+    static void captureCoverage() throws IOException {
+        /*
+         * This is calling after each instance of this subclass, not after every single test.
+         * I think the better way to do this will be jacoco in tcpserver mode and AfterAll calling the
+         * command to dump & append to the existing file.
+         *
+         * This file will be cleaned as part of the maven cleanup before a new run
+         *
+         * Then we have called it before the container is automatically exited.
+         */
+        ExecDumpClient jacocoClient = new ExecDumpClient();
+        ExecFileLoader dump = jacocoClient.dump(API_CONTAINER.getHost(), API_CONTAINER.getMappedPort(6300));
+        dump.save(new File("./target/coverage-reports/jacoco-it.exec"), true);
     }
 }
